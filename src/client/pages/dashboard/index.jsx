c42b153ded11a76d01fb5a504f750e273448e368
Onah Victor <victoronah.dev@gmail.com>
2024-08-30 08:08:41 +0100

    Updated logic for displaying the dashboard

--- src/client/pages/dashboard/index.jsx
+++ src/client/pages/dashboard/index.jsx
@@ -5,48 +5,13 @@ import { Link } from "react-router-dom";
 import { AppContext } from "../../App";
 import { useContext } from "react";
 import { Helmet } from "react-helmet";
-
-const transactionsHistory = [
-	{
-		date: "8th Jan, 2024",
-		type: "withdrawal",
-		amount: "150.00",
-		status: "completed"
-	},
-	{
-		date: "9th Jan, 2024",
-		type: "deposit",
-		amount: "500.00",
-		status: "pending"
-	},
-	{
-		date: "10th Jan, 2024",
-		type: "withdrawal",
-		amount: "200.00",
-		status: "failed"
-	},
-	{
-		date: "11th Jan, 2024",
-		type: "deposit",
-		amount: "300.00",
-		status: "completed"
-	},
-	{
-		date: "12th Jan, 2024",
-		type: "withdrawal",
-		amount: "50.00",
-		status: "completed"
-	},
-	{
-		date: "13th Jan, 2024",
-		type: "deposit",
-		amount: "700.00",
-		status: "pending"
-	}
-];
+import computeTotalDeposits from "../../utils/compute-total-deposits";
+import computeTotalWithdrawals from "../../utils/compute-total-withdrawals";
 
 const Dashboard = () => {
 	const { state } = useContext(AppContext);
+	const deposits = computeTotalDeposits(state.user.transactions || []);
+	const withdrawals = computeTotalWithdrawals(state.user.transactions || []);
 
 	return (
 		<>
@@ -65,7 +30,7 @@ const Dashboard = () => {
 					<div>
 						<p className="text-xs">Available balance</p>
 						<h2 className="font-black text-lg">
-							ETH {state.user.balance || "0.00"}
+							ETH {Number(deposits - withdrawals).toFixed(2)}
 						</h2>
 					</div>
 					<div className="flex justify-center items-center gap-4">
@@ -85,15 +50,11 @@ const Dashboard = () => {
 			<section className="text-sm p-4 flex gap-4">
 				<div className="bg-gradient-to-r from-green-500 to-green-600 flex-1 rounded-md space-y-2 p-4">
 					<h4 className="text-xs">Total deposit</h4>
-					<p className="font-bold">
-						ETH {state.user.totalDeposit || "0.00"}
-					</p>
+					<p className="font-bold">ETH {deposits.toFixed(2)}</p>
 				</div>
 				<div className="bg-gradient-to-r from-red-500 to-red-600 flex-1 rounded-md space-y-2 p-4">
 					<h4 className="text-xs">Total withdrawal</h4>
-					<p className="font-bold">
-						ETH {state.user.totalWithdrawal || "0.00"}
-					</p>
+					<p className="font-bold">ETH {withdrawals.toFixed(2)}</p>
 				</div>
 			</section>
 			<section className="p-4 space-y-4 flex-1">
@@ -104,44 +65,43 @@ const Dashboard = () => {
 					</Link>
 				</div>
 				<div className="text-sm space-y-2">
-					{state.user.transactionsHistory ? (
-						transactionsHistory.map((transaction, index) => (
-							<Link
-								key={index}
-								to={`./transactions/${transaction.id}`}
-								className="active:scale-[.98] flex items-center gap-4 backdrop-blur-sm bg-white bg-opacity-20 p-2 rounded-lg border border-slate-300">
-								<div className="flex gap-1 items-center text-xs">
-									<BiCalendar /> {transaction.date}
-								</div>
-								<div className="flex-1">
-									<small className="capitalize">
-										{transaction.type}
-									</small>
-									<p className="font-bold">
-										{transaction.type === "withdrawal"
-											? "-"
-											: "+"}
-										ETH{" "}
-										{Number(
-											transaction.amount
-										).toLocaleString()}
-									</p>
-								</div>
-								<div>
-									<p
-										className={`${
-											transaction.status === "completed"
-												? "text-blue-400"
-												: transaction.status ===
-												  "pending"
-												? "text-yellow-400"
-												: "text-red-300"
-										} capitalize text-xs`}>
-										{transaction.status}
-									</p>
-								</div>
-							</Link>
-						))
+					{state.user.transactions ? (
+						state.user.transactions
+							.slice(0, 7)
+							.map((transaction, index) => (
+								<Link
+									key={index}
+									to={`./transactions/${transaction._id}`}
+									className="active:scale-[.98] flex items-center justify-between gap-4 backdrop-blur-sm bg-white bg-opacity-20 p-2 rounded-lg border border-slate-300">
+									<div className="flex flex-1 gap-1 items-center text-xs">
+										<BiCalendar />{" "}
+										{new Date(
+											transaction.date
+										).toLocaleDateString()}
+									</div>
+									<div className="flex-1">
+										<p
+											className={`${
+												transaction.type === "deposit"
+													? "text-green-300"
+													: "text-red-300"
+											}`}>
+											{transaction.type === "withdrawal"
+												? "-"
+												: "+"}
+											ETH{" "}
+											{Number(
+												transaction.amount
+											).toLocaleString()}
+										</p>
+									</div>
+									<div className="flex-1 text-right">
+										<p className="capitalize">
+											{transaction.type}
+										</p>
+									</div>
+								</Link>
+							))
 					) : (
 						<div className="text-center p-4 rounded-lg bg-white bg-opacity-20 border border-slate-300 backdrop-blur-md">
 							No transactions to show
