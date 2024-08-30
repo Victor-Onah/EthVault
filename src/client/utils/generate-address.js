6542bdbd66e21171136d861c1ff4218f60e506fd
Onah Victor <victoronah.dev@gmail.com>
2024-08-30 08:07:08 +0100

    Added utility function for generating random wallet addreses

---
+++ src/client/utils/generate-address.js
@@ -0,0 +1,15 @@
+const generateETHAddress = () => {
+	const characters =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let address = "0x";
+
+	for (let i = 0; i <= 40; i++) {
+		address += characters.charAt(
+			Math.floor(Math.random() * characters.length)
+		);
+	}
+
+	return address;
+};
+
+export default generateETHAddress;
